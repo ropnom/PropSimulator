c1d5e59854a9c224d119b4baaaeb3d139df33a07
Ropnom <ropnom5291@gmail.com>
2015-07-17 14:52:36 +0200

    test commit

---
+++ src/eetac/propsimulator/propsimconsole.java
@@ -0,0 +1,11 @@
+package eetac.propsimulator;
+
+public class propsimconsole {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hellow world!! \n this is the firts line of PropSim");
+	}
+
+}
