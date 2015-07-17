3f25d6ceaba56c333e366e53014787a6d86005f9
miquel15 <miquelsolera@gmail.com>
2015-07-17 15:18:21 +0200

    my firts commit

--- src/eetac/propsimulator/propsimconsole.java
+++ src/eetac/propsimulator/propsimconsole.java
@@ -6,6 +6,7 @@ public class propsimconsole {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hellow world!! \n this is the firts line of PropSim");
+		System.out.print("Holaaa");
+		System.out.println("end");
 	}
-
 }
