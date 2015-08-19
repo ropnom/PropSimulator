670d0c993048d066ce619d55090ad71aab0ea507
Ropnom <ropnom5291@gmail.com>
2015-08-19 19:33:17 +0200

    more thing gui

---
+++ src/eetac/gui/VariablePanel.java
@@ -0,0 +1,28 @@
+package eetac.gui;
+
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class VariablePanel extends JPanel {
+
+	/**
+	 * This compoennt is a panel with checkbox, label, texbox for varaible data.
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	JCheckBox variable;
+	JTextField value;
+	
+	public VariablePanel() {
+
+		this.variable = new JCheckBox("Nombrevariable", false);
+		this.value = new JTextField("0.15");
+		
+		value.setEnabled(false);
+		
+		this.add(variable);
+		this.add(value);
+	}
+
+}
