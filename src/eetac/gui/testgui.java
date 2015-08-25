013371645c9b393e80a6ac0ba275a7e34985579a
miquel15 <miquelsolera@gmail.com>
2015-08-25 18:12:58 +0200

    commit amb visual

---
+++ src/eetac/gui/testgui.java
@@ -0,0 +1,47 @@
+package eetac.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class testgui {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					testgui window = new testgui();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public testgui() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		VariablePanel mypanel = new VariablePanel();
+		frame.getContentPane().add(mypanel, BorderLayout.CENTER);
+	}
+
+}
