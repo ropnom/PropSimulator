4ade73c88d5d0f35d0bcbefb3fc9bb3cbd3b559a
Ropnom <ropnom5291@gmail.com>
2015-08-27 23:02:42 +0200

    gui

--- src/eetac/gui/working/Projet.java
+++ src/eetac/gui/working/Projet.java
@@ -3,10 +3,23 @@ package eetac.gui.working;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JInternalFrame;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JProgressBar;
+import javax.swing.JLabel;
 
 public class Projet {
 
-	private JFrame frame;
+	private JInternalFrame frmNewProject;
 
 	/**
 	 * Launch the application.
@@ -16,7 +29,7 @@ public class Projet {
 			public void run() {
 				try {
 					Projet window = new Projet();
-					window.frame.setVisible(true);
+					window.frmNewProject.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +48,46 @@ public class Projet {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmNewProject = new JInternalFrame();
+		frmNewProject.setTitle("New Project");
+		frmNewProject.setBounds(100, 100, 870, 549);
+		frmNewProject.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JToolBar toolBar_up = new JToolBar();
+		frmNewProject.getContentPane().add(toolBar_up, BorderLayout.NORTH);
+		
+		JButton btnNewButton = new JButton("New button");
+		toolBar_up.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		toolBar_up.add(btnNewButton_1);
+		
+		JToolBar toolBar_down = new JToolBar();
+		frmNewProject.getContentPane().add(toolBar_down, BorderLayout.SOUTH);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		toolBar_down.add(btnNewButton_3);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		toolBar_down.add(lblNewLabel);
+		
+		JProgressBar progressBar = new JProgressBar();
+		toolBar_down.add(progressBar);
+		
+		JPanel panel = new JPanel();
+		frmNewProject.getContentPane().add(panel, BorderLayout.WEST);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		panel.add(tabbedPane);
+		
+		JButton btnNewButton_2 = new JButton("New button");
+		tabbedPane.addTab("New tab", null, btnNewButton_2, null);
+		
+		JTextArea textArea = new JTextArea();
+		tabbedPane.addTab("New tab", null, textArea, null);
+		
+		JPanel panel_1 = new JPanel();
+		frmNewProject.getContentPane().add(panel_1, BorderLayout.CENTER);
 	}
 
 }
--- src/eetac/gui/working/PropSimulator.java
+++ src/eetac/gui/working/PropSimulator.java
@@ -3,10 +3,19 @@ package eetac.gui.working;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.Toolkit;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+import javax.swing.JDesktopPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PropSimulator {
 
-	private JFrame frame;
+	private JFrame frmPropSimulator;
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class PropSimulator {
 			public void run() {
 				try {
 					PropSimulator window = new PropSimulator();
-					window.frame.setVisible(true);
+					window.frmPropSimulator.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +44,48 @@ public class PropSimulator {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmPropSimulator = new JFrame();
+		frmPropSimulator.setIconImage(Toolkit.getDefaultToolkit().getImage("G:\\Usuarios\\Rodrigo\\workspace\\PropSimulator\\ico\\Jet_Engine_copy.png"));
+		frmPropSimulator.setTitle("Prop Simulator");
+		frmPropSimulator.setBounds(100, 100, 1163, 742);
+		frmPropSimulator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmPropSimulator.setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		frmPropSimulator.getContentPane().add(desktopPane, BorderLayout.CENTER);
+		
+		JMenuItem mntmNewProject = new JMenuItem("New Project");
+		mntmNewProject.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Projet a = new Projet();
+				//desktopPane.add(a);
+			}
+		});
+		
+		mnFile.add(mntmNewProject);
+		
+		JMenuItem mntmOpenProjet = new JMenuItem("Open Projet");
+		mnFile.add(mntmOpenProjet);
+		
+		JMenuItem mntmExit = new JMenuItem("Exit");
+		mnFile.add(mntmExit);
+		
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+		
+		JMenuItem mntmPreferences = new JMenuItem("Preferences");
+		mnEdit.add(mntmPreferences);
+		
+		JMenu mnAbout = new JMenu("About");
+		menuBar.add(mnAbout);
+		
+	
 	}
 
 }
